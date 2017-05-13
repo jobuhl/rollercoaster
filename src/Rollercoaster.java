0b37d9c184f0cb2986f80aa8e522428da17eaccd
urskenner <urskenner@web.de>
2017-05-13 16:39:16 +0200

    -> GUI first steps

---
+++ src/Rollercoaster.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Jojo on 13.05.17.
+ */
+public class Rollercoaster {
+
+    private static GUI gui;
+
+    public static void main(String[] args) {
+        gui = new GUI();
+        gui.setVisible(true);
+    }
+
+}
+
+
+
